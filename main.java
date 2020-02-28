a1704f19ad5b740b189ac5b135a3fc7639716ac1
Brian Hulbert <Brian.hulbert@me.com>
2020-02-28 14:09:56 -0500

    added a movement method that does U and U'

--- main.java
+++ main.java
@@ -85,13 +85,64 @@ public class main{
         }
         String[][][] cube = new String[][][]{white,blue,yellow,green,orange,red};
         System.out.println("IsSolved: "+checkSolved(cube));
+        printCube(cube);
+        while(true){
+            String movement = scn.nextLine();
+            cube = move(cube,movement);
+            System.out.println("Moved the cube: "+movement);
+            printCube(cube);
+        }
+    }
+    public static void printCube(String[][][] cube){
+        for(String[][] face: cube){
+            for(int y = 0; y < face[0].length; y++){
+                for(int x = 0; x < face.length; x++){
+                    System.out.print(face[x][y]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println("\n");
+        }
+    }
+    //assumes yellow is on top && looking at red face
+    public static String[][][] move(String[][][] c,String move){
+        if(move.equals("U'")){
+            int[] top = new int[]{5,3,4,1};
+            String[] temp = new String[]{c[top[0]][0][0],c[top[0]][1][0],c[top[0]][2][0]};
+            for(int i = 0; i < top.length-1; i++){
+                String[] replaced = new String[]{c[top[i+1]][0][0],c[top[i+1]][1][0],c[top[i+1]][2][0]};
+                c[top[i+1]][0][0] = temp[0];
+                c[top[i+1]][1][0] = temp[1];
+                c[top[i+1]][2][0] = temp[2];
+                temp = replaced;
+            }
+            c[5][0][0] = temp[0];
+            c[5][1][0] = temp[1];
+            c[5][2][0] = temp[2];
+        }
+        else if(move.equals("U")){
+            int[] top = new int[]{5,1,4,3};
+            String[] temp = new String[]{c[top[0]][0][0],c[top[0]][1][0],c[top[0]][2][0]};
+            for(int i = 0; i < top.length-1; i++){
+                String[] replaced = new String[]{c[top[i+1]][0][0],c[top[i+1]][1][0],c[top[i+1]][2][0]};
+                c[top[i+1]][0][0] = temp[0];
+                c[top[i+1]][1][0] = temp[1];
+                c[top[i+1]][2][0] = temp[2];
+                temp = replaced;
+            }
+            c[5][0][0] = temp[0];
+            c[5][1][0] = temp[1];
+            c[5][2][0] = temp[2];
+        }
+        return c;
     }
     public static boolean checkSolved(String[][][] cube){
         //white,blue,yellow,green,orange,red
         for(int i = 0; i < cube.length; i++){
             for(int x = 0; x < cube[i].length;x++){
                 for(int y = 0; y < cube[i][x].length;y++){
-                    if((i==0 && !cube[i][x][y].equals("w"))||(i==1 && !cube[i][x][y].equals("b"))||(i==2 && !cube[i][x][y].equals("y"))||(i==3 && !cube[i][x][y].equals("g"))||(i==4 && !cube[i][x][y].equals("o"))||(i==5 && !cube[i][x][y].equals("w"))){
+                    if((i==0 && !cube[i][x][y].equals("w"))||(i==1 && !cube[i][x][y].equals("b"))||(i==2 && !cube[i][x][y].equals("y"))||(i==3 && !cube[i][x][y].equals("g"))||(i==4 && !cube[i][x][y].equals("o"))||(i==5 && !cube[i][x][y].equals("r"))){
+                        System.out.println("Face: "+i);
                         return false;
                     }
                 }
